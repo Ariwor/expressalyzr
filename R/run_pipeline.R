62f8dbe05209a105c211bb6dc897009c9b027b1d
Timothy Frei <frei.timothy@gmail.com>
2020-05-25 20:53:54 +0200

    add description to run_pipeline.R

--- R/run_pipeline.R
+++ R/run_pipeline.R
@@ -1,3 +1,12 @@
+#' Run the data processing pipline
+#'
+#' Run the data processing pipline that links together other functions in the
+#'  expressalyzr package.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 run_pipeline <- function() {
 
 }
