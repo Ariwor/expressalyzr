7f45cd5d59a1bd0bb2e1089ff55202d6e26a9c3e
Timothy Frei <frei.timothy@gmail.com>
2020-05-25 20:06:17 +0200

    Initial commit

---
+++ R/expressalyzr.R
@@ -0,0 +1,11 @@
+#' expressalyzr: A package for automated gating and compensation of flow
+#' cytometry data.
+#'
+#' The expressalyzr package provides useful functions and a general pipeline for
+#' automated gating and compensation of flow cytometry data.
+#'
+#' @section Foo functions: The foo functions ...
+#'
+#' @docType package
+#' @name expressalyzr
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(expressalyzr)
+
+test_check("expressalyzr")
