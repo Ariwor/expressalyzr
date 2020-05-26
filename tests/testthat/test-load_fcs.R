3a5b78d94145afc8cf1fd0c7e75b6c77e934aae5
Timothy Frei <frei.timothy@gmail.com>
2020-05-26 22:24:05 +0200

    add unit test skeleton to test for correct return value of load_fcs.R

--- tests/testthat/test-load_fcs.R
+++ tests/testthat/test-load_fcs.R
@@ -1,4 +1,6 @@
-context("error handling")
+context("test fcs file loading")
+
+## errors if non-existant file path or no .fcs files in the directory
 
 test_that("non-existing file paths throw an error", {
   test_file_path <- "./does_not_exist/"
@@ -17,4 +19,11 @@ test_that("existing file paths with no .fcs files throw an error", {
   expect_error(load_fcs(test_file_path), "^.* not contain any .*$")
   # clean up
   expect_equal(unlink(test_file_path, recursive = TRUE), 0)
+})
+
+## return value
+
+test_that("a gating set is returned", {
+  test_file_path <- system.file("extdata", "example_fcs_files", package = "expressalyzr", mustWork = TRUE)
+
 })
