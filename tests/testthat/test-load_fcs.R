8e916e79d964590d6b307630e572183e1a90b14a
Timothy Frei <frei.timothy@gmail.com>
2020-05-26 21:52:05 +0200

    update error tests for load_fcs.R

--- tests/testthat/test-load_fcs.R
+++ tests/testthat/test-load_fcs.R
@@ -1,6 +1,20 @@
 context("error handling")
 
 test_that("non-existing file paths throw an error", {
-  test_file_path <- "./doesnotexist/"
+  test_file_path <- "./does_not_exist/"
+  # create dummy directory just to remove again
+  expect_equal(dir.create(test_file_path), TRUE)
+  expect_equal(unlink(test_file_path, recursive = TRUE), 0)
+  expect_equal(dir.exists(test_file_path), FALSE)
   expect_error(load_fcs(test_file_path), "^.* not exist .*$")
+})
+
+test_that("existing file paths with no .fcs files throw an error", {
+  test_file_path <- "./no_fcs_files"
+  # create dummy directory for testing
+  expect_equal(dir.create(test_file_path), TRUE)
+  expect_equal(dir.exists(test_file_path), TRUE)
+  expect_error(load_fcs(test_file_path), "^.* not contain any .*$")
+  # clean up
+  expect_equal(unlink(test_file_path, recursive = TRUE), 0)
 })
