57d1dc5019c01e4a05fda711d80c4b096bf6bc2d
Timothy Frei <frei.timothy@gmail.com>
2020-11-01 15:15:19 +0100

    Updated tests, specifically test-load_fcs.R

--- tests/testthat/test-load_fcs.R
+++ tests/testthat/test-load_fcs.R
@@ -1,21 +1,19 @@
-context("test fcs file loading")
-
 #### errors if non-existant file path or no .fcs files in the directory ####
 
 test_that("non-existing file paths throw an error", {
   test_file_path <- "./does_not_exist/"
   # create dummy directory just to remove again
-  expect_equal(dir.create(test_file_path), TRUE)
+  expect_true(dir.create(test_file_path))
   expect_equal(unlink(test_file_path, recursive = TRUE), 0)
-  expect_equal(dir.exists(test_file_path), FALSE)
+  expect_false(dir.exists(test_file_path))
   expect_error(load_fcs(test_file_path), "^.* not exist .*$")
 })
 
 test_that("existing file paths with no .fcs files throw an error", {
   test_file_path <- "./no_fcs_files"
   # create dummy directory for testing
-  expect_equal(dir.create(test_file_path), TRUE)
-  expect_equal(dir.exists(test_file_path), TRUE)
+  expect_true(dir.create(test_file_path))
+  expect_true(dir.exists(test_file_path))
   expect_error(load_fcs(test_file_path), "^.* not contain any .*$")
   # clean up
   expect_equal(unlink(test_file_path, recursive = TRUE), 0)
@@ -23,7 +21,8 @@ test_that("existing file paths with no .fcs files throw an error", {
 
 #### return value  ####
 
-test_that("a gating set is returned", {
-  test_file_path <- system.file("extdata", "example_fcs_files", package = "expressalyzr", mustWork = TRUE)
-
+test_that("a cytoset is returned", {
+  test_file_path <- system.file("extdata", "example_fcs_files",
+                                package = "expressalyzr", mustWork = TRUE)
+  expect_equal(is(load_fcs(test_file_path)), c("cytoset", "flowSet"))
 })
