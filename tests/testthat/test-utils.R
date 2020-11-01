1019f972b1709bfab0b642c84a1d0db840575c28
Timothy Frei <frei.timothy@gmail.com>
2020-11-01 23:40:42 +0100

    fix tests after fixing load_fcs function

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -5,7 +5,7 @@
 test_that("non-existing file paths throw an error", {
   test_dir_path <- file.path(".", "does_not_exist")
   # create dummy directory just to remove again
-  expect_true(dir.create(test_dir_path))
+  expect_true(dir.create(file.path(test_dir_path, "data"), recursive = TRUE))
   expect_equal(unlink(test_dir_path, recursive = TRUE), 0)
   expect_false(dir.exists(test_dir_path))
   expect_error(load_fcs(test_dir_path), "^.* not exist .*$")
@@ -14,7 +14,7 @@ test_that("non-existing file paths throw an error", {
 test_that("existing file paths with no .fcs files throw an error", {
   test_dir_path <- file.path(".", "no_fcs_files")
   # create dummy directory for testing
-  expect_true(dir.create(test_dir_path))
+  expect_true(dir.create(file.path(test_dir_path, "data"), recursive = TRUE))
   expect_true(dir.exists(test_dir_path))
   expect_error(load_fcs(test_dir_path), "^.* not contain any .*$")
   # clean up
