3ee5e2163a9b9bfc0fcefcaf9ac76a8cf0af4870
Timothy Frei <frei.timothy@gmail.com>
2020-11-01 14:58:07 +0100

    Avoid segfault from C stack overflow.

--- R/load_fcs.R
+++ R/load_fcs.R
@@ -10,7 +10,19 @@
 #' @examples
 #' path <- system.file("extdata", "example_fcs_files", package = "expressalyzr", mustWork = TRUE)
 #' gating_set <- load_fcs(path)
+
 load_fcs <- function(file_path) {
+  fcs_files <- list.files(path = file_path, pattern = "^.*\\.fcs$", full.names = TRUE)
+  if (identical(fcs_files, character(0))) {
+    stop("The specified file path does either not exist or does not contain any .fcs files.")
+  } else {
+    gating_set <- flowWorkspace::load_cytoset_from_fcs(files = fcs_files)
+  }
+  return(gating_set)
+}
+
+# this function causes a segfault form C stack overflow on my computer
+load_fcs_ncdf <- function(file_path) {
   fcs_files <- list.files(path = file_path, pattern = "^.*\\.fcs$", full.names = TRUE)
   if (identical(fcs_files, character(0))) {
     stop("The specified file path does either not exist or does not contain any .fcs files.")
