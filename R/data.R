01aea838ce121804f1e08919f55c54a383ded4f0
Timothy Frei <frei.timothy@gmail.com>
2020-11-03 10:32:30 +0100

    update data

--- R/data.R
+++ R/data.R
@@ -1,12 +1,11 @@
 #' Data for molecules of equivalent fluorescin (MEFL) per peak.
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' Data for MEFL per peak for RCP-30-5A Lot #AK04.
 #'
 #' @format A data table with 8 rows and 2 variables:
 #' \describe{
 #'   \item{Peak}{number of peak, from dimmest to brightest}
 #'   \item{MEFL}{molecules of equivalent fluorescin, in molecules}
 #' }
-#' @source \url{https://spherotech.com/2020%20Product%20Detail%20Pages/Spherotech%20Rainbow%20Calibration%20Particles.pdf}
+#' @source Sheet that came with the beads.
 "mefl_dt"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,5 @@
 peaks <- 1:8
-mefl <- c(NA, 789, 1896, 4872, 15619, 47116, 143912, 333068)
+mefl <- c(NA, 771, 2106, 6262, 15183, 45292, 136258, 291042)
 
 mefl_dt <- data.table::data.table(Peak = peaks,
                                   MEFL = mefl
